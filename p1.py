0ed8b885a7cd7d0a68a381c7441c1471e6a1a4ab
Harshali81 <156742295+Harshali81@users.noreply.github.com>
2024-11-10 22:12:47 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,34 +1,26 @@
-def fibonacci(n_terms):
-    n_1 = 0
-    n_2 =1
-    step_count = 0  # To track the number of steps
-    sequence = []  # To store the Fibonacci sequence
-
-    if n_terms <= 0:
-        print("Please enter a positive integer, the given number is not valid")
-        return None, step_count, sequence  # Returning None, step count, and an empty sequence
-
-    elif n_terms == 1:
-        sequence.append(n_1)
-        print(f"The Fibonacci number at position {n_terms} is: {n_1}")
-        print(f"The Fibonacci sequence up to {n_terms} is: {sequence}")
-        return n_1, step_count, sequence
-
+def recur(n):
+    if n <= 1:
+        return n
     else:
-        while step_count < n_terms:
-            sequence.append(n_1)  # Add the current Fibonacci number to the sequence
-            if step_count == n_terms - 1:
-                print(f"The Fibonacci number at position {n_terms} is: {n_1}")
-            nth = n_1 + n_2
-            n_1 = n_2
-            n_2 = nth
-            
-            step_count += 1
+        return recur(n - 1) + recur(n - 2)
 
-    print(f"The Fibonacci sequence up to {n_terms} is: {sequence}")
-    return n_1, step_count, sequence
+def iterative(n):
+    a = 0
+    b = 1
+    print(a)
+    print(b)
+    for i in range(2, n):
+        print(a + b)
+        a, b = b, a + b
 
-# Test the function
-n_terms = int(input("Enter the position of Fibonacci number you want to find: "))
-fibonacci_number, steps, sequence = fibonacci(n_terms)
-print(f"Number of steps taken to compute the Fibonacci number: {steps}")
+if __name__ == "__main__":
+    num = int(input("Enter the nth number for series: "))
+    if num <= 0:
+        print("Please enter a positive integer")
+    else:
+        print("Fibonacci sequence with recursion:")
+        for i in range(num):
+            print(recur(i))
+        
+        print("Fibonacci series with Iteration:")
+        iterative(num)
